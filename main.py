6c4ab00c386956a26bf9616cdfa0fddb2053283d
Shreyas Acharya <130966494+shreyas-acharya-amagi@users.noreply.github.com>
2024-05-20 10:56:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import json
 
 if __name__ == "__main__":
-  print(json.dumps([{"first": 1, "second": 2}, {"first": 3, "second": 4}]), seperators=(",", ":"))
+  print(json.dumps([{"first": 1, "second": 2}, {"first": 3, "second": 4}]), separators=(",", ":"))
